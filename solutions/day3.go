1249d54c9519f85d2eaf8f5c02e426df439b9f60
Li Xin Rui <li.xinrui3@gmail.com>
2024-12-03 20:22:56 -0500

    day 3 part 2

--- solutions/day3.go
+++ solutions/day3.go
@@ -9,14 +9,43 @@ import (
 
 func RunDay3(lines []string) {
 	fullstring := strings.Join(lines, "\n")
+	part1 := day3Part1(fullstring)
+	part2 := day3Part2(fullstring)
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
+
+func day3Part1(fullstring string) int {
 	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	matches := re.FindAllStringSubmatch(fullstring, -1)
-	fmt.Println(matches)
 	sum := 0
 	for _, expr := range matches {
 		a, _ := strconv.Atoi(expr[1])
 		b, _ := strconv.Atoi(expr[2])
 		sum += a * b
 	}
-	fmt.Println(sum)
+	return sum
+}
+
+func day3Part2(fullstring string) int {
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(fullstring, -1)
+	isEnabled := true
+	sum := 0
+	for _, expr := range matches {
+		if expr[0] == "do()" {
+			isEnabled = true
+			continue
+		}
+		if expr[0] == "don't()" {
+			isEnabled = false
+			continue
+		}
+		if isEnabled {
+			a, _ := strconv.Atoi(expr[1])
+			b, _ := strconv.Atoi(expr[2])
+			sum += a * b
+		}
+	}
+	return sum
 }
