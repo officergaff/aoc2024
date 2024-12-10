83404b576d4081d58d5ef795d375210a81f87380
Li Xin Rui <li.xinrui3@gmail.com>
2024-12-10 03:58:12 -0500

    day 8 part 2

--- solutions/day8.go
+++ solutions/day8.go
@@ -23,9 +23,30 @@ type Point struct {
 	X int
 }
 
-func getAntiNodes(a Point, b Point) []Point {
-	yDist, xDist := a.Y-b.Y, a.X-b.X
-	return []Point{{a.Y + yDist, a.X + xDist}, {b.Y - yDist, b.X - xDist}}
+func getAntiNodes1(a Point, b Point) []Point {
+	// Creating a vector from a->b
+	yDist, xDist := b.Y-a.Y, b.X-a.X
+	return []Point{{a.Y - yDist, a.X - xDist}, {b.Y + yDist, b.X + xDist}}
+}
+
+func getAntiNodes2(a Point, b Point, maxRow int, maxCol int) []Point {
+	yDist, xDist := b.Y-a.Y, b.X-a.X
+	nodes := []Point{}
+	for i := 0; ; i-- {
+		node := Point{a.Y - (yDist * i), a.X - (xDist * i)}
+		if !(0 <= node.Y && node.Y < maxRow && 0 <= node.X && node.X < maxCol) {
+			break
+		}
+		nodes = append(nodes, node)
+	}
+	for i := 1; ; i++ {
+		node := Point{a.Y + (yDist * i), a.X + (xDist * i)}
+		if !(0 <= node.Y && node.Y < maxRow && 0 <= node.X && node.X < maxCol) {
+			break
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
 
 func addNodes(nodeMap map[Point]bool, nodes []Point, maxRow int, maxCol int) {
@@ -36,8 +57,7 @@ func addNodes(nodeMap map[Point]bool, nodes []Point, maxRow int, maxCol int) {
 	}
 }
 
-func day8Part1(graph [][]string) int {
-	maxRow, maxCol := len(graph), len(graph[0])
+func getSignalCoords(graph [][]string) map[string][]Point {
 	signalCoords := map[string][]Point{}
 	for j, line := range graph {
 		for i, cell := range line {
@@ -47,14 +67,38 @@ func day8Part1(graph [][]string) int {
 			signalCoords[cell] = append(signalCoords[cell], Point{j, i})
 		}
 	}
+	return signalCoords
+}
+
+func day8Part1(graph [][]string) int {
+	maxRow, maxCol := len(graph), len(graph[0])
 	antiNodes := map[Point]bool{}
+	signalCoords := getSignalCoords(graph)
 	for _, antennas := range signalCoords {
 		for curr, antenna := range antennas {
 			for idx, other := range antennas {
 				if curr == idx {
 					continue
 				}
-				nodes := getAntiNodes(antenna, other)
+				nodes := getAntiNodes1(antenna, other)
+				addNodes(antiNodes, nodes, maxRow, maxCol)
+			}
+		}
+	}
+	return len(antiNodes)
+}
+
+func day8Part2(graph [][]string) int {
+	maxRow, maxCol := len(graph), len(graph[0])
+	antiNodes := map[Point]bool{}
+	signalCoords := getSignalCoords(graph)
+	for _, antennas := range signalCoords {
+		for curr, antenna := range antennas {
+			for idx, other := range antennas {
+				if curr == idx {
+					continue
+				}
+				nodes := getAntiNodes2(antenna, other, maxRow, maxCol)
 				addNodes(antiNodes, nodes, maxRow, maxCol)
 			}
 		}
@@ -70,4 +114,6 @@ func RunDay8(data []byte) {
 	}
 	part1 := day8Part1(graph)
 	fmt.Println(part1)
+	part2 := day8Part2(graph)
+	fmt.Println(part2)
 }
